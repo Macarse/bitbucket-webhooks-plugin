0f0ec0d96bbe7b0913b54d620da1cf2cbe49a0d8
Joseph Petersen <josephp90@gmail.com>
2017-08-21 19:18:59 +0200

    json remove empty

--- src/main/java/nl/topicus/bitbucket/events/BitbucketServerPullRequestEvent.java
+++ src/main/java/nl/topicus/bitbucket/events/BitbucketServerPullRequestEvent.java
@@ -27,8 +27,10 @@ import nl.topicus.bitbucket.model.pullrequest.BitbucketServerPullRequest;
 import nl.topicus.bitbucket.model.repository.BitbucketServerRepository;
 import nl.topicus.bitbucket.model.repository.BitbucketServerRepositoryOwner;
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_EMPTY)
 public class BitbucketServerPullRequestEvent
 {
     private BitbucketServerRepositoryOwner actor;
